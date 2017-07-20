67a0a37b3bd5aac04296d1e2d641249c0058258f
Vasil Primov <vasilevs1996@gmail.com>
2017-07-20 15:21:31 +0300

    finnished(i think) with character creation

--- TryToGame/TryToGame/Program.cs
+++ TryToGame/TryToGame/Program.cs
@@ -17,13 +17,15 @@ namespace TryToGame
             string Gender;
             string Race;
             string Class;
+            string[] Inventory = new string[20];
+            string Input;
 
 
 
             //Character creation:
             do
             {
-                Console.Clear();
+                //Console.Clear();
                 Console.WriteLine("Please choose a gender from below:");
                 Console.WriteLine("Male/Female");
                 Gender = Console.ReadLine().ToUpper();
@@ -31,13 +33,13 @@ namespace TryToGame
                 {
                     correct = 1;
                 }
-                if (Gender == "FEMALE")
+                else if (Gender == "FEMALE")
                 {
                     correct = 1;
                 }
                 else
                 {
-
+                    Console.Clear();
                 }
             } while (correct == 0);
             correct = 0;
@@ -51,15 +53,105 @@ namespace TryToGame
                 Console.WriteLine("Dwarf");
                 Console.WriteLine("Elf");
                 Console.WriteLine("Orc");
-                Console.WriteLine("Psychopath");
+                //Console.WriteLine("Psychopath");
                 Race = Console.ReadLine().ToUpper();
-                if (Race == "HUMAN" || Race == "DWARF" || Race == "ELF" || Race == "ORC" || Race == "PSYCHOPATH")
+                if (Race == "HUMAN")
                 {
-                    correct = 1;
+                    Console.WriteLine("This race gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 1 Point");
+                    Console.WriteLine("Two Handed Weapons. 1 Point");
+                    Console.WriteLine("Ranged Weapons.     1 Point");
+                    Console.WriteLine("Magical Attack.     1 Point");
+                    Console.WriteLine("Is this the race you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+
+                if (Race == "DWARF")
+                {
+                    Console.WriteLine("This race gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 1 Point");
+                    Console.WriteLine("Two Handed Weapons. 2 Point");
+                    Console.WriteLine("Ranged Weapons.     1 Point");
+                    Console.WriteLine("Magical Attack.     0 Point");
+                    Console.WriteLine("Is this the race you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+
+                if (Race == "ELF")
+                {
+                    Console.WriteLine("This race gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 0 Point");
+                    Console.WriteLine("Two Handed Weapons. 1 Point");
+                    Console.WriteLine("Ranged Weapons.     2 Point");
+                    Console.WriteLine("Magical Attack.     1 Point");
+                    Console.WriteLine("Is this the race you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+
+                if (Race == "ORC")
+                {
+                    Console.WriteLine("This race gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 2 Point");
+                    Console.WriteLine("Two Handed Weapons. 2 Point");
+                    Console.WriteLine("Ranged Weapons.     0 Point");
+                    Console.WriteLine("Magical Attack.     0 Point");
+                    Console.WriteLine("Is this the race you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+                if (Race == "PSYCHOPATH")
+                {
+                    Console.WriteLine("This race gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 0 Point");
+                    Console.WriteLine("Two Handed Weapons. 0 Point");
+                    Console.WriteLine("Ranged Weapons.     2 Point");
+                    Console.WriteLine("Magical Attack.     2 Point");
+                    Console.WriteLine("This is the hidden Race, so you surely want to keep it, but just in case...");
+                    Console.WriteLine("Is this the race you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
                 }
                 else
                 {
-
+                    Console.Clear();
                 }
             } while (correct == 0);
             correct = 0;
@@ -69,27 +161,114 @@ namespace TryToGame
             //Class creation:
             do
             {
-                Console.Clear();
+                //Console.Clear();
                 Console.WriteLine("Please choose a class from below:");
                 Console.WriteLine("Warrior");
                 Console.WriteLine("Hunter");
                 Console.WriteLine("Mage");
                 Console.WriteLine("Thief");
-                Console.WriteLine("Whore");
+                //Console.WriteLine("Bitch");
                 Class = Console.ReadLine().ToUpper();
-                if (Class == "WARRIOR" || Class == "HUNTER" || Class == "MAGE" || Class == "THIEF" || Class == "WHORE")
+                if (Class == "WARRIOR")
                 {
-                    correct = 1;
+                    Console.WriteLine("This class gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 2 Point");
+                    Console.WriteLine("Two Handed Weapons. 2 Point");
+                    Console.WriteLine("Ranged Weapons.     0 Point");
+                    Console.WriteLine("Magical Attack.     0 Point");
+                    Console.WriteLine("Is this the class you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+                if (Class == "HUNTER")
+                {
+                    Console.WriteLine("This class gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 1 Point");
+                    Console.WriteLine("Two Handed Weapons. 1 Point");
+                    Console.WriteLine("Ranged Weapons.     2 Point");
+                    Console.WriteLine("Magical Attack.     0 Point");
+                    Console.WriteLine("Is this the class you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+                if (Class == "MAGE")
+                {
+                    Console.WriteLine("This class gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 1 Point");
+                    Console.WriteLine("Two Handed Weapons. 0 Point");
+                    Console.WriteLine("Ranged Weapons.     1 Point");
+                    Console.WriteLine("Magical Attack.     2 Point");
+                    Console.WriteLine("Is this the class you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+                if (Class == "THIEF")
+                {
+                    Console.WriteLine("This class gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 2 Point");
+                    Console.WriteLine("Two Handed Weapons. 0 Point");
+                    Console.WriteLine("Ranged Weapons.     1 Point");
+                    Console.WriteLine("Magical Attack.     1 Point");
+                    Console.WriteLine("Is this the class you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
+                }
+                if (Class == "BITCH")
+                {
+                    Console.WriteLine("This class gives a bonus to the following stats:");
+                    Console.WriteLine("One Handed Weapons. 1 Point");
+                    Console.WriteLine("Two Handed Weapons. 1 Point");
+                    Console.WriteLine("Ranged Weapons.     0 Point");
+                    Console.WriteLine("Magical Attack.     2 Point");
+                    Console.WriteLine("This is the hidden Class, so you surely want to keep it, but just in case...");
+                    Console.WriteLine("Is this the class you wish to play? Enter Yes/No Below:");
+                    Input = Console.ReadLine().ToUpper();
+                    if (Input == "YES")
+                    {
+                        correct = 1;
+                    }
+                    if (Input == "NO")
+                    {
+                        correct = 0;
+                    }
                 }
                 else
                 {
-
+                    Console.Clear();
                 }
             } while (correct == 0);
             correct = 0;
 
             //Confirm that is what you chose:
-            Console.Clear();
+            //Console.Clear();
             Console.WriteLine("Your gender is: " + Gender);
             Console.WriteLine("Your race is: " + Race);
             Console.WriteLine("Your class is: " + Class);
@@ -180,7 +359,7 @@ namespace TryToGame
                 PlayerRanged += 1;
                 PlayerMagic += 1;
             }
-            if (Class == "WHORE")
+            if (Class == "BITCH")
             {
                 PlayerOneHanded += 1;
                 PlayerTwoHanded += 1;
@@ -194,14 +373,61 @@ namespace TryToGame
             Console.WriteLine("Your two handed weapon skills: " + PlayerTwoHanded);
             Console.WriteLine("Your ranged weapon skills:     " + PlayerRanged);
             Console.WriteLine("Your magic skills:             " + PlayerMagic);
-            Console.Write("Press enter to continue...");
+            Console.WriteLine("Press enter to continue...");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            {
+
+            }
+            Console.Clear();
+
+
+            Inventory[0] = "a";
+            Inventory[1] = "severed penis";
+            Inventory[2] = "turtle";
+
+            //INVENTORY TEST
+            Console.WriteLine("Inventory test: Type inventory below");
+            Input = Console.ReadLine().ToUpper();
+            while(correct == 0)
+            {
+                if(Input == "INVENTORY")
+                {
+                    //Console.Clear();
+                    int free = 0;
+                    Console.WriteLine("Your inventory contains:");
+                    for (int Arraycount = 0; Arraycount < 20; Arraycount++)
+                    {
+                        if (Inventory[Arraycount] != null)
+                        {
+                            Console.WriteLine("----------");
+                            Console.WriteLine("| " + Inventory[Arraycount]);
+
+                        }
+                        else
+                        {
+                            free++;
+                        }
+                    }
+                    Console.WriteLine("----------\n\n");
+                    Console.WriteLine("You have " + free + " free inventory spaces.\n\n");
+                    correct = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input, please try again");
+                    Input = Console.ReadLine().ToUpper();
+                }
+
+            }
+            correct = 0;
+            //INVENTPRY TEST END
+            Console.WriteLine("Press enter to continue...");
             while (Console.ReadKey(true).Key != ConsoleKey.Enter)
             {
 
             }
 
 
-            
         }
     }
 }
